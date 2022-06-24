7bf6486a2a3856b52c6f46e5633c134c237ff98b
doesnt1matter <89796576+doesnt1matter@users.noreply.github.com>
2022-06-24 13:05:35 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,239 @@
+function testWebP(callback) {
+
+    var webP = new Image();
+    webP.onload = webP.onerror = function () {
+        callback(webP.height == 2);
+    };
+    webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+
+function addClassOnClick(clickedElement, changedElements, addClass) {
+    const firstElement = document.querySelector(clickedElement)
+
+    firstElement.addEventListener("click", () => {
+        for (let index = 0; index < changedElements.length; index++) {
+            document.querySelector(changedElements[index]).classList.toggle(addClass)
+        }
+    })
+}
+
+const dottedDecor = document.querySelectorAll("._dotted")
+for (let index = 0; index < dottedDecor.length; index++) {
+    const decor = dottedDecor[index]
+
+    for (let index = Math.floor(decor.clientWidth / 5); index > 0; index--) {
+        const round = document.createElement("div")
+        decor.appendChild(round)
+    }
+}
+
+addClassOnClick(".header__burger", [".header__links", ".burger", "body"], "_active")
+
+addClassOnClick(".screen-advantages__button",
+    ["#screen-advantages-element_1",
+        "#screen-advantages-element_2",
+        "#screen-advantages-element_3",
+        "#screen-advantages-element_4",
+        "#screen-advantages-element_5",
+        ".screen-advantages__button",
+        ".screen-advantages__description"],
+    "_active")
+
+const popupButton = document.querySelector(".main-info-telephone__call")
+popupButton.addEventListener("click", () => {
+    document.body.classList.toggle('_lock')
+    document.querySelector(".popup").classList.toggle("_active")
+
+    if (document.querySelector(".header__links").classList.contains("_active")) {
+        document.querySelector(".header__links").classList.add("_lock")
+    }
+})
+
+const popupClose = document.querySelector(".popup__close")
+popupClose.addEventListener("click", () => {
+    document.body.classList.toggle('_lock')
+    document.querySelector(".popup").classList.toggle("_active")
+
+    if (document.querySelector(".header__links").classList.contains("_active")) {
+        document.querySelector(".header__links").classList.remove("_lock")
+    }
+})
+
+window.addEventListener("DOMContentLoaded", function () {
+    [].forEach.call(document.querySelectorAll('.tel'), function (input) {
+        var keyCode;
+        function mask(event) {
+            event.keyCode && (keyCode = event.keyCode);
+            var pos = this.selectionStart;
+            if (pos < 3) event.preventDefault();
+            var matrix = "+7 (___) ___ ____",
+                i = 0,
+                def = matrix.replace(/\D/g, ""),
+                val = this.value.replace(/\D/g, ""),
+                new_value = matrix.replace(/[_\d]/g, function (a) {
+                    return i < val.length ? val.charAt(i++) || def.charAt(i) : a
+                });
+            i = new_value.indexOf("_");
+            if (i != -1) {
+                i < 5 && (i = 3);
+                new_value = new_value.slice(0, i)
+            }
+            var reg = matrix.substr(0, this.value.length).replace(/_+/g,
+                function (a) {
+                    return "\\d{1," + a.length + "}"
+                }).replace(/[+()]/g, "\\$&");
+            reg = new RegExp("^" + reg + "$");
+            if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) this.value = new_value;
+            if (event.type == "blur" && this.value.length < 5) this.value = ""
+        }
+
+        input.addEventListener("input", mask, false);
+        input.addEventListener("focus", mask, false);
+        input.addEventListener("blur", mask, false);
+        input.addEventListener("keydown", mask, false)
+
+    });
+
+});
+
+function DynamicAdapt(type) {
+    this.type = type;
+}
+DynamicAdapt.prototype.init = function () {
+    const _this = this;
+    // массив объектов
+    this.оbjects = [];
+    this.daClassname = "_dynamic_adapt_";
+    // массив DOM-элементов
+    this.nodes = document.querySelectorAll("[data-da]");
+
+    // наполнение оbjects объктами
+    for (let i = 0; i < this.nodes.length; i++) {
+        const node = this.nodes[i];
+        const data = node.dataset.da.trim();
+        const dataArray = data.split(",");
+        const оbject = {};
+        оbject.element = node;
+        оbject.parent = node.parentNode;
+        оbject.destination = document.querySelector(dataArray[0].trim());
+        оbject.breakpoint = dataArray[1] ? dataArray[1].trim() : "767";
+        оbject.place = dataArray[2] ? dataArray[2].trim() : "last";
+        оbject.index = this.indexInParent(оbject.parent, оbject.element);
+        this.оbjects.push(оbject);
+    }
+
+    this.arraySort(this.оbjects);
+
+    // массив уникальных медиа-запросов
+    this.mediaQueries = Array.prototype.map.call(this.оbjects, function (item) {
+        return '(' + this.type + "-width: " + item.breakpoint + "px)," + item.breakpoint;
+    }, this);
+    this.mediaQueries = Array.prototype.filter.call(this.mediaQueries, function (item, index, self) {
+        return Array.prototype.indexOf.call(self, item) === index;
+    });
+
+    // навешивание слушателя на медиа-запрос
+    // и вызов обработчика при первом запуске
+    for (let i = 0; i < this.mediaQueries.length; i++) {
+        const media = this.mediaQueries[i];
+        const mediaSplit = String.prototype.split.call(media, ',');
+        const matchMedia = window.matchMedia(mediaSplit[0]);
+        const mediaBreakpoint = mediaSplit[1];
+
+        // массив объектов с подходящим брейкпоинтом
+        const оbjectsFilter = Array.prototype.filter.call(this.оbjects, function (item) {
+            return item.breakpoint === mediaBreakpoint;
+        });
+        matchMedia.addListener(function () {
+            _this.mediaHandler(matchMedia, оbjectsFilter);
+        });
+        this.mediaHandler(matchMedia, оbjectsFilter);
+    }
+};
+DynamicAdapt.prototype.mediaHandler = function (matchMedia, оbjects) {
+    if (matchMedia.matches) {
+        for (let i = 0; i < оbjects.length; i++) {
+            const оbject = оbjects[i];
+            оbject.index = this.indexInParent(оbject.parent, оbject.element);
+            this.moveTo(оbject.place, оbject.element, оbject.destination);
+        }
+    } else {
+        for (let i = 0; i < оbjects.length; i++) {
+            const оbject = оbjects[i];
+            if (оbject.element.classList.contains(this.daClassname)) {
+                this.moveBack(оbject.parent, оbject.element, оbject.index);
+            }
+        }
+    }
+};
+DynamicAdapt.prototype.moveTo = function (place, element, destination) {
+    element.classList.add(this.daClassname);
+    if (place === 'last' || place >= destination.children.length) {
+        destination.insertAdjacentElement('beforeend', element);
+        return;
+    }
+    if (place === 'first') {
+        destination.insertAdjacentElement('afterbegin', element);
+        return;
+    }
+    destination.children[place].insertAdjacentElement('beforebegin', element);
+}
+DynamicAdapt.prototype.moveBack = function (parent, element, index) {
+    element.classList.remove(this.daClassname);
+    if (parent.children[index] !== undefined) {
+        parent.children[index].insertAdjacentElement('beforebegin', element);
+    } else {
+        parent.insertAdjacentElement('beforeend', element);
+    }
+}
+DynamicAdapt.prototype.indexInParent = function (parent, element) {
+    const array = Array.prototype.slice.call(parent.children);
+    return Array.prototype.indexOf.call(array, element);
+};
+DynamicAdapt.prototype.arraySort = function (arr) {
+    if (this.type === "min") {
+        Array.prototype.sort.call(arr, function (a, b) {
+            if (a.breakpoint === b.breakpoint) {
+                if (a.place === b.place) {
+                    return 0;
+                }
+
+                if (a.place === "first" || b.place === "last") {
+                    return -1;
+                }
+
+                if (a.place === "last" || b.place === "first") {
+                    return 1;
+                }
+
+                return a.place - b.place;
+            }
+
+            return a.breakpoint - b.breakpoint;
+        });
+    } else {
+        Array.prototype.sort.call(arr, function (a, b) {
+            if (a.breakpoint === b.breakpoint) {
+                if (a.place === b.place) {
+                    return 0;
+                }
+
+                if (a.place === "first" || b.place === "last") {
+                    return 1;
+                }
+
+                if (a.place === "last" || b.place === "first") {
+                    return -1;
+                }
+
+                return b.place - a.place;
+            }
+
+            return b.breakpoint - a.breakpoint;
+        });
+        return;
+    }
+};
+
+const da = new DynamicAdapt("max");
+da.init();
